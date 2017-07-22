f8e6f89eac35898a805d7cd8a3a4d46c4dd582cb
KristineWolf <kristine_wolf@web.de>
2017-07-22 20:33:20 +0200

    project

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class ChangeColor : MonoBehaviour, ITrackableEventHandler
+{
+    //delivered cube of the scene
+    public GameObject cube;
+    Color green = new Color(0,1,0, 1);
+    Color red = new Color(1, 0, 0, 1);
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    //checks for status changes
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            //Trackable found
+            cube.GetComponent<Renderer>().sharedMaterial.color = green;
+        }
+        else
+        {
+           // Trackable lost
+            cube.GetComponent<Renderer>().sharedMaterial.color = red;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# AR
+Studienleistung für die Vorlesung "Anwendungsschwerpunkt: Digitalisierung und digitale Gesellschaft"
+Universität Regensburg
+
+Judith Amrhein und Kristine Wolf
